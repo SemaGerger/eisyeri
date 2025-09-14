a5c5c0f137c97244f5fc5be5c18ca89d43d4d31a
Sema Gerger <sky.gerger1@gmail.com>
2025-09-14 17:58:29 +0300

    deneme

--- src/components/button/CTAButton.js
+++ src/components/button/CTAButton.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const CTAButton = () => {
   return (
-    <Link to="/formPage" target="_blank" rel="noopener noreferrer">
+    <Link to="/form" target="_blank" rel="noopener noreferrer">
       <button className="px-8 py-3 bg-red-600 text-white font-bold rounded-lg shadow-lg transform hover:-translate-y-1 hover:scale-105 transition duration-300">
         İşyerinizi Kaydedin
       </button>
@@ -11,20 +11,3 @@ const CTAButton = () => {
 };
 
 export default CTAButton;
-
-
-/*const CTAButton = () => {
-  return (
-    <a
-      href="https://ulakbel.bcekmece.bel.tr/WebBasvuru/esitisyeri#/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <button className="px-8 py-3 bg-red-600 text-white font-bold rounded-lg shadow-lg transform hover:-translate-y-1 hover:scale-105 transition duration-300">
-        İşyerinizi Kaydedin
-      </button>
-    </a>
-  );
-};
-
-export default CTAButton;*/
